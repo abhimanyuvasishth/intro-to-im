15a5b644ec58c7859d948f1215830a78b67b7022
abhimanyu <abhimanyu.vasishth@gmail.com>
2016-10-03 17:32:58 +0400

    simple code for arduino-python communication

--- assignments/stupid_pet_trick/communicate.py
+++ assignments/stupid_pet_trick/communicate.py
@@ -4,10 +4,6 @@ from time import sleep
 import serial
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
-counter = 32
-
-while counter < 35:
-     counter +=1
-     ser.write(str(chr(counter))) # Convert the decimal number to ASCII then send it to the Arduino
-     print ser.readline() # Read the newest output from the Arduino
-     sleep(2) # Delay for one tenth of a second
+ser.write(str(chr(97))) # Convert the decimal number to ASCII then send it to the Arduino ('a')
+print ser.readline() # Read the newest output from the Arduino
+sleep(0.1) # Delay for one tenth of a second
